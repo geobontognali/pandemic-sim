171e844da0d9ce25b8c7f531e75cb0a2d1002220
Geo Bontognali <geo.bontognali@gmail.com>
2021-03-09 23:58:57 +0100

    first graphic simulation added (curses). now the virus transmits on contact. README added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Usage
+Just change the global variables and run the script
+
+# Todos
+- Define probability of infection after contact
+- Improve curses quality
+- Import vars from INI
+- Main Class
+
+# Dependencies
+## Python 3.x
+Installation: choco install python -y
+## Curses
+Installation: pip install windows-curses
--- pandemic.py
+++ pandemic.py
@@ -1,13 +1,21 @@
-from os import system
 from random import randrange
-system("cls")
+import time
+import curses
 
 ################################
 class Person:
-    def __init__(self, id, virus):
-        self.id = id
-        self.virus = virus
-        self.pos = [randrange(0,51),randrange(0,51)]
+    def __init__(self, pid):
+        self.pid = pid
+        self.virus = False
+        self.pos = [randrange(0,cityWidth+1),randrange(0,cityHeight+1)]
+        self.symbol = "O"
+
+    def infect(self):
+        self.virus = True
+        self.symbol = "#"
+    def cure(self):
+        self.virus = False
+        self.symbol = "O"
 
     def move(self):
         # Move Randomly, but keep inside city boundries
@@ -25,16 +33,18 @@ class Person:
             self.pos[1] += randrange(-1,1)
         else:
             self.pos[1] += randrange(-1,2)
-        return self.pos
     
     def transmitVirus(self, people):
-        if not self.virus:
+        if not self.virus: # if not sick dont do anything
             return
         for person in people:
-            if(person.id == self.id):
-                continue # skip myself
+            if(person.pid == self.pid): # skip myself
+                continue 
+            if(person.virus): # if already infected, also skip
+                continue
             if(person.pos == self.pos):
-                print("[VIRUS] I ("+ str(self.id) + ") found a person ("+ str(person.id)+") with me at " + str(self.pos) + " on day " + str(day))
+                print("[VIRUS] I ("+ str(self.pid) + ") found a person ("+ str(person.pid)+") with me at " + str(self.pos) + " on step " + str(step))
+                person.infect()
 
     def position(self):
         print("I am "+str(self.id)+" at position " + str(self.pos[0]) + " " + str(self.pos[1]))
@@ -42,34 +52,52 @@ class Person:
 
 ### MAIN ###
 ## Globals
-cityWidth = 50
-cityHeight = 50
-population = 200
-duration = 60
+cityWidth = 130
+cityHeight = 20
+population = 25
+infected = 5
+duration = 1000
 
-## Init
+## Simulation Init
 people = []
-for pID in range(population):
-    people.append(Person(pID, False))
-people[0].virus = True
+for pid in range(population): ## Generate population
+    people.append(Person(pid))
+for infected in range(infected): ## Patient zeros
+    people[infected].infect()
 
-"""
-print("Staring positions: ")
-for person in people:
-        person.position()
-"""
+# Curses GUI init
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(True)
+
+# Simulation
 print("Simulation begins...")
-day = 0
-for day in range(duration): # People do move once per day
+step = 0
+for step in range(duration): # People do move once per step
     #print("Day " + str(day))
+    stdscr.erase()
     for person in people:
         person.move()
+        stdscr.addstr(person.pos[1],person.pos[0], person.symbol)
     for person in people:
         person.transmitVirus(people)
-print("Simulation ended")
+    stdscr.refresh()
+    time.sleep(0.05)
 
-"""
-print("Final  positions:")
+# Curses GUI cleanup (wait for keypress)
+stdscr.getkey()
+curses.nocbreak()
+stdscr.keypad(False)
+curses.echo()
+curses.endwin()
+
+## Recap
+print("Simulation ended")
+print("Total people in the simulation: " + str(population))
+print("People infected at the beginning: " + str(infected))
+infected = 0
 for person in people:
-        person.position()
-"""
+        if(person.virus):
+            infected += 1
+print("People infected at the end: " + str(infected) )
