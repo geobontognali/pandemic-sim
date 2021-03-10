a1fe928a7d878a7f761ea0685dac6f3fa11b1060
Geo Bontognali <geo.bontognali@gmail.com>
2021-03-11 00:21:04 +0100

    first functional gui with 4 separate windows refreshing independently. simulation code integrated

--- pandemic.py
+++ pandemic.py
@@ -5,52 +5,28 @@ import time
 
 ### GLOBALS
 version = "0.1a"
-population = 20
+population = 100
 initiallyInfected = 1
-duration = 50
+duration = 1000
+fps = 10
 
 
 ### Main ##########################
 def main(stdscr):
-    # Text
-    topText = "Simulation running"
-    footerText = "Press 'q' to exit | Version " + version
-
-    # Init Screens and colors
-    stdscr.clear()
-    stdscr.refresh()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-    # Simulation Plane
-    global simPlaneWidth 
+    # Define Sizes
+    global topWinHeight
+    global bottomWinHeight
+    global footerHeight
     global simPlaneHeight
-    simPlaneWidth = stdscr.getmaxyx()[0] - 11
-    simPlaneHeight = stdscr.getmaxyx()[1] - 1
+    global simPlaneWidth 
+    topWinHeight = 1
+    bottomWinHeight = 4
+    footerHeight = 1
+    simPlaneWidth = (stdscr.getmaxyx()[1] - 1)
+    simPlaneHeight = stdscr.getmaxyx()[0] - (topWinHeight + bottomWinHeight + footerHeight)
 
-    # Top bar
-    topWin = curses.newwin(1,simPlaneHeight, 0, 0)
-    topWin.attron(curses.color_pair(1))  
-    topWin.addstr(0,0,topText)
-    topWin.refresh()
-    # Center space
-    centerWin = curses.newwin(simPlaneWidth,simPlaneHeight, 1, 0)
-    centerWin.addstr(0,0,"center")
-    centerWin.refresh()
-    # Bottom space
-    bottomWin = curses.newwin(9,simPlaneHeight, simPlaneWidth+1, 0)
-    bottomWin.addstr(0,0,"bottom")
-    bottomWin.refresh()
-    # Footer
-    footerWin = curses.newwin(1,simPlaneHeight, simPlaneWidth+10, 0)
-    footerWin.attron(curses.color_pair(3))    
-    footerWin.addstr(0,0," " + footerText + " " * (simPlaneHeight - len(footerText) - 2))   
-    footerWin.refresh()
-
-
-    stdscr.getkey()
-
+    # Init GUI
+    GUI = WindowMgr(stdscr)
 
     # Initialize objects
     global people
@@ -61,39 +37,75 @@ def main(stdscr):
         people[i].infect()
 
     # Begin simulation
-    print("Simulation begins...")
+    GUI.setTopText("Simulation running")
+    
     global frame
     frame = 0
     for frame in range(duration): # People do move once per frame
-        stdscr.erase()
         for person in people:
             person.move()
-            stdscr.addch(person.pos[1],person.pos[0], person.symbol)
+            GUI.addToSimPlane(person.pos[1],person.pos[0], person.symbol)
         for person in people:
             person.transmitVirus(people)
-        stdscr.refresh()
-        time.sleep(0.1)
+        GUI.refreshSimPlane()
+        time.sleep(1/fps)
 
     ## Terminate simulation by keypress
+    GUI.setTopText("Simulation ended. Press any key to quit")
     stdscr.getkey()
-
-    ## Recap
-    print("Simulation ended")
-    print("Total people in the simulation: " + str(population))
-    print("People infected at the beginning: " + str(initiallyInfected))
-    infected = 0
-    for person in people:
-            if(person.virus):
-                infected += 1
-    print("People infected at the end: " + str(infected) )
 
 
 ### Classes ##########################
+# Handles the curses calls and controls
+class WindowMgr:
+    def __init__(self, stdscr):
+        ## Init curses, clear Screen, defines colours
+        stdscr.clear()
+        stdscr.refresh()
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        self.topText = "Press any key to start the simulation"
+        self.footerText = "Press 'q' to exit | Version " + version
+        ## Init windows    
+        # Top bar
+        self.topWin = curses.newwin(topWinHeight,simPlaneWidth, 0, 0)
+        self.topWin.attron(curses.color_pair(1))  
+        self.topWin.addstr(0,0,self.topText)
+        self.topWin.refresh()
+        # Center space
+        self.centerWin = curses.newwin(simPlaneHeight,simPlaneWidth, topWinHeight, 0)
+        self.centerWin.refresh()
+        # Bottom space
+        self.bottomWin = curses.newwin(bottomWinHeight,simPlaneWidth, topWinHeight+simPlaneHeight, 0)
+        self.bottomWin.addstr(0,0,"STATS:")
+        self.bottomWin.refresh()
+        # Footer
+        self.footerWin = curses.newwin(footerHeight,simPlaneWidth, topWinHeight+simPlaneHeight+bottomWinHeight, 0)
+        self.footerWin.attron(curses.color_pair(3))    
+        self.footerWin.addstr(0,0," " + self.footerText + " " * (simPlaneWidth - len(self.footerText) - 2)) 
+        self.footerWin.refresh()
+        stdscr.getkey()
+
+    # Changes and refreshes the text of the top bar
+    def setTopText(self, message):
+        self.topWin.clear()
+        self.topWin.addstr(0,0,message)
+        self.topWin.refresh()
+
+    # Edit the simulation plane
+    def addToSimPlane(self,y,x,symbol):
+        self.centerWin.addch(y,x,symbol)
+    def refreshSimPlane(self):
+        self.centerWin.refresh()
+        self.centerWin.clear() # Prep for the next frame
+
+# Its the main moving entity
 class Person:
     def __init__(self, pid):
         self.pid = pid
         self.virus = False
-        self.pos = [randrange(0,simPlaneHeight+1),randrange(0,simPlaneWidth+1)]
+        self.pos = [randrange(2,simPlaneWidth-2),randrange(2,simPlaneHeight-2)]
         self.symbol = "O"
 
     def infect(self):
@@ -105,17 +117,17 @@ class Person:
 
     def move(self):
         # Move Randomly, but keep inside city boundries
-        # X
+        # X (width)
         if(self.pos[0] == 0):
             self.pos[0] += randrange(0,2)
-        elif(self.pos[0] == simPlaneHeight):
+        elif(self.pos[0] == simPlaneWidth-2):
             self.pos[0] += randrange(-1,1)
         else:
             self.pos[0] += randrange(-1,2)
-        # Y
+        # Y (height)
         if(self.pos[1] == 0):
             self.pos[1] += randrange(0,2)
-        elif(self.pos[1] == simPlaneWidth):
+        elif(self.pos[1] == simPlaneHeight-2):
             self.pos[1] += randrange(-1,1)
         else:
             self.pos[1] += randrange(-1,2)
@@ -129,12 +141,7 @@ class Person:
             if(person.virus): # if already infected, also skip
                 continue
             if(person.pos == self.pos):
-                print("[VIRUS] I ("+ str(self.pid) + ") found a person ("+ str(person.pid)+") with me at " + str(self.pos) + " on frame " + str(frame))
                 person.infect()
-
-    def position(self):
-        print("I am "+str(self.id)+" at position " + str(self.pos[0]) + " " + str(self.pos[1]))
-
 
 ### INIT
 curses.wrapper(main)
