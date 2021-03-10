9e27bd5d221959686589bb743096137428a1d824
Geo Bontognali <geo.bontognali@gmail.com>
2021-03-10 23:03:02 +0100

    curses windows and bars added

--- pandemic.py
+++ pandemic.py
@@ -1,13 +1,99 @@
+### IMPORT
 from random import randrange
-import time
 import curses
+import time
 
-################################
+### GLOBALS
+version = "0.1a"
+population = 20
+initiallyInfected = 1
+duration = 50
+
+
+### Main ##########################
+def main(stdscr):
+    # Text
+    topText = "Simulation running"
+    footerText = "Press 'q' to exit | Version " + version
+
+    # Init Screens and colors
+    stdscr.clear()
+    stdscr.refresh()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # Simulation Plane
+    global simPlaneWidth 
+    global simPlaneHeight
+    simPlaneWidth = stdscr.getmaxyx()[0] - 11
+    simPlaneHeight = stdscr.getmaxyx()[1] - 1
+
+    # Top bar
+    topWin = curses.newwin(1,simPlaneHeight, 0, 0)
+    topWin.attron(curses.color_pair(1))  
+    topWin.addstr(0,0,topText)
+    topWin.refresh()
+    # Center space
+    centerWin = curses.newwin(simPlaneWidth,simPlaneHeight, 1, 0)
+    centerWin.addstr(0,0,"center")
+    centerWin.refresh()
+    # Bottom space
+    bottomWin = curses.newwin(9,simPlaneHeight, simPlaneWidth+1, 0)
+    bottomWin.addstr(0,0,"bottom")
+    bottomWin.refresh()
+    # Footer
+    footerWin = curses.newwin(1,simPlaneHeight, simPlaneWidth+10, 0)
+    footerWin.attron(curses.color_pair(3))    
+    footerWin.addstr(0,0," " + footerText + " " * (simPlaneHeight - len(footerText) - 2))   
+    footerWin.refresh()
+
+
+    stdscr.getkey()
+
+
+    # Initialize objects
+    global people
+    people = []
+    for pid in range(population): ## Generate population
+        people.append(Person(pid))
+    for i in range(initiallyInfected): ## Patient zeros
+        people[i].infect()
+
+    # Begin simulation
+    print("Simulation begins...")
+    global frame
+    frame = 0
+    for frame in range(duration): # People do move once per frame
+        stdscr.erase()
+        for person in people:
+            person.move()
+            stdscr.addch(person.pos[1],person.pos[0], person.symbol)
+        for person in people:
+            person.transmitVirus(people)
+        stdscr.refresh()
+        time.sleep(0.1)
+
+    ## Terminate simulation by keypress
+    stdscr.getkey()
+
+    ## Recap
+    print("Simulation ended")
+    print("Total people in the simulation: " + str(population))
+    print("People infected at the beginning: " + str(initiallyInfected))
+    infected = 0
+    for person in people:
+            if(person.virus):
+                infected += 1
+    print("People infected at the end: " + str(infected) )
+
+
+### Classes ##########################
 class Person:
     def __init__(self, pid):
         self.pid = pid
         self.virus = False
-        self.pos = [randrange(0,cityWidth+1),randrange(0,cityHeight+1)]
+        self.pos = [randrange(0,simPlaneHeight+1),randrange(0,simPlaneWidth+1)]
         self.symbol = "O"
 
     def infect(self):
@@ -22,14 +108,14 @@ class Person:
         # X
         if(self.pos[0] == 0):
             self.pos[0] += randrange(0,2)
-        elif(self.pos[0] == cityWidth):
+        elif(self.pos[0] == simPlaneHeight):
             self.pos[0] += randrange(-1,1)
         else:
             self.pos[0] += randrange(-1,2)
         # Y
         if(self.pos[1] == 0):
             self.pos[1] += randrange(0,2)
-        elif(self.pos[1] == cityHeight):
+        elif(self.pos[1] == simPlaneWidth):
             self.pos[1] += randrange(-1,1)
         else:
             self.pos[1] += randrange(-1,2)
@@ -43,61 +129,12 @@ class Person:
             if(person.virus): # if already infected, also skip
                 continue
             if(person.pos == self.pos):
-                print("[VIRUS] I ("+ str(self.pid) + ") found a person ("+ str(person.pid)+") with me at " + str(self.pos) + " on step " + str(step))
+                print("[VIRUS] I ("+ str(self.pid) + ") found a person ("+ str(person.pid)+") with me at " + str(self.pos) + " on frame " + str(frame))
                 person.infect()
 
     def position(self):
         print("I am "+str(self.id)+" at position " + str(self.pos[0]) + " " + str(self.pos[1]))
 
 
-### MAIN ###
-## Globals
-cityWidth = 130
-cityHeight = 20
-population = 25
-infected = 5
-duration = 1000
-
-## Simulation Init
-people = []
-for pid in range(population): ## Generate population
-    people.append(Person(pid))
-for infected in range(infected): ## Patient zeros
-    people[infected].infect()
-
-# Curses GUI init
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(True)
-
-# Simulation
-print("Simulation begins...")
-step = 0
-for step in range(duration): # People do move once per step
-    #print("Day " + str(day))
-    stdscr.erase()
-    for person in people:
-        person.move()
-        stdscr.addstr(person.pos[1],person.pos[0], person.symbol)
-    for person in people:
-        person.transmitVirus(people)
-    stdscr.refresh()
-    time.sleep(0.05)
-
-# Curses GUI cleanup (wait for keypress)
-stdscr.getkey()
-curses.nocbreak()
-stdscr.keypad(False)
-curses.echo()
-curses.endwin()
-
-## Recap
-print("Simulation ended")
-print("Total people in the simulation: " + str(population))
-print("People infected at the beginning: " + str(infected))
-infected = 0
-for person in people:
-        if(person.virus):
-            infected += 1
-print("People infected at the end: " + str(infected) )
+### INIT
+curses.wrapper(main)
